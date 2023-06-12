1a33d15dfc032a19c15cff6ef834790aef8c70cf
VitorJosee <95343929+VitorJosee@users.noreply.github.com>
2023-06-12 11:50:10 -0300

    first commit

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD6exsn8CElRmZJmAdXo4aiYY9ZebAXAHs",
+    authDomain: "littlemixmobile.firebaseapp.com",
+    databaseURL: "https://littlemixmobile-default-rtdb.firebaseio.com",
+    projectId: "littlemixmobile",
+    storageBucket: "littlemixmobile.appspot.com",
+    messagingSenderId: "341192228717",
+    appId: "1:341192228717:web:4272eaa5e89d0a54022464"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
